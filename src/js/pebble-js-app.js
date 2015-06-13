9199842eea191551bfbae9447d006c1270fe0432
Christian Speckner <cnspeckn@googlemail.com>
2015-06-13 23:06:59 +0200

    Redo JS.

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,52 +1,144 @@
+(function(window) {
+'use strict';
+
 var eventTypes = {
-    feed: 0,
-    0: 'feed',
-    diaper_change: 1,
-    1: 'diaper_change',
-    sleep_start: 2,
-    2: 'sleep_start',
-    sleep_stop: 3,
-    3: 'sleep_stop'
-};
+        feed: 0,
+        0: 'feed',
+        diaper_change: 1,
+        1: 'diaper_change',
+        sleep_start: 2,
+        2: 'sleep_start',
+        sleep_stop: 3,
+        3: 'sleep_stop'
+    },
+    allEventTypes = [eventTypes.feed, eventTypes.diaper_change, eventTypes.sleep_start, eventTypes.sleep_stop];
 
 var MESSAGE_KEY_EVENT_BLOB = 0;
 
-function unserializeEvent(buffer, index) {
-    var type = buffer[index] in eventTypes ? eventTypes[buffer[index]] : null;
+var events = [],
+    eventsByTypeAndTimestamp = {
+        0: {},
+        1: {},
+        2: {},
+        3: {}
+    };
 
-    if (type !== null) {
-        return {
-            type: type,
-            timestamp: 
-                ((buffer[index + 1] & 0xFF)      ) |
-                ((buffer[index + 2] & 0xFF) << 8 ) |
-                ((buffer[index + 3] & 0xFF) << 16) |
-                ((buffer[index + 4] & 0xFF) << 24)
-        };
+var storage;
+
+///////////////////////////////////////////////////////////////////////////////
+
+function Event(type, timestamp) {
+    this.type = type;
+    this.timestamp = timestamp;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+function LocalStorageAdapter() {
+    var me = this;
+
+    me.unserialized = {};
+
+    allEventTypes.forEach(function(eventType) {
+        var serializedTimestamps = window.localStorage.getItem(eventType),
+            timestamps;
+
+        if (serializedTimestamps === null) serializedTimestamps = "[]";
+
+        try {
+            timestamps = JSON.parse(serializedTimestamps);
+        } catch (e) {
+            console.log("Unable to parse local storage - corrupt data? Clearing...");
+            window.localStorage.removeItem(eventType);
+            
+            timestamps = [];
+        }
+
+        me.unserialized[eventType] = timestamps;
+    });
+}
+
+LocalStorageAdapter.prototype.getAll = function() {
+    var me = this,
+        events = [];
+
+    allEventTypes.forEach(function(eventType) {
+        me.unserialized[eventType].forEach(function(timestamp) {
+            events.push(new Event(eventType, timestamp));
+        });
+    });
+
+    return events;
+};
+
+LocalStorageAdapter.prototype.add = function(e) {
+    this.unserialized[e.type].push(e.timestamp);
+    window.localStorage.setItem(e.type, JSON.stringify(this.unserialized[e.type]));
+};
+
+LocalStorageAdapter.prototype.clear = function() {
+    window.localStorage.clear();
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+function unserializeEvent(buffer, index) {
+    var type = buffer[index] in eventTypes ? buffer[index] : null,
+        timestamp;
+
+   if (type !== null) {
+        timestamp = ((buffer[index + 1] & 0xFF)      ) |
+                    ((buffer[index + 2] & 0xFF) << 8 ) |
+                    ((buffer[index + 3] & 0xFF) << 16) |
+                    ((buffer[index + 4] & 0xFF) << 24);
+
+        return new Event(type, timestamp);
     } else {
         return null;
     }
 }
 
+function addNewEvent(e) {
+    if (e && !eventsByTypeAndTimestamp[e.type][e.timestamp]) {
+        events.push(e);
+        eventsByTypeAndTimestamp[e.type][e.timestamp] = e;
+        storage.add(e);
+
+        return true;
+    }
+
+    return false;
+}
+
 Pebble.addEventListener("ready",
 	function(e) {
 		console.log("Pebby JavaScript ready!");
+
+        storage = new LocalStorageAdapter();
+
+        storage.getAll().forEach(addNewEvent);
+
+        console.log("loaded " + events.length + " events from local storage");
     }
 );
 
 Pebble.addEventListener("appmessage",
 	function(e) {
-		console.log("Received message: " + JSON.stringify(e.payload));
+        console.log('incoming message');
 
         if (MESSAGE_KEY_EVENT_BLOB in e.payload) {
             var buffer = e.payload[MESSAGE_KEY_EVENT_BLOB],
-                eventCount = buffer[0];
+                eventCount = buffer[0],
+                addedEvents = 0;
 
             console.log('received ' + eventCount + ' events');
-            
+
             for (var i = 0; i < eventCount; i++) {
-                console.log(JSON.stringify(unserializeEvent(buffer, 1 + 5*i), null, '  '));
+                if (addNewEvent(unserializeEvent(buffer, 1 + 5*i))) addedEvents++;
             }
+
+            console.log('added ' + addedEvents + ' events, now at ' +
+                events.length + ' events total');
         } else {
             console.log('invalid message');
         }
@@ -55,7 +147,31 @@ Pebble.addEventListener("appmessage",
 
 Pebble.addEventListener("showConfiguration",
 	function(e) {
-		//Pebble.openURL("http://www.epicvortex.com/pebble/pebby.html?#" + serializeStorage(window.localStorage));
+        var rawData = {
+            0: [],
+            1: [],
+            2: [],
+            3: []
+        };
+
+        console.log('show config');
+
+        events.forEach(function(event) {
+            rawData[event.type].push(event.timestamp);
+        });
+
+        var serializedData = JSON.stringify({
+            1: JSON.stringify(rawData[eventTypes.feed]),
+            2: JSON.stringify(rawData[eventTypes.diaper_change]),
+            3: JSON.stringify(rawData[eventTypes.sleep_start]),
+            4: JSON.stringify(rawData[eventTypes.sleep_stop])
+        });
+
+        var url = "http://www.epicvortex.com/pebble/pebby.html?#" + serializedData;
+
+        console.log(url);
+
+		Pebble.openURL(url);
 	}
 );
 
@@ -68,5 +184,4 @@ Pebble.addEventListener("webviewclosed",
 	}
 );
 
-
-
+})(window);
